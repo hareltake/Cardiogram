dfb91dd6a729e6421974b56c2ca9dfc17c5320b5
guogaoyang <guogaoyang@157C779A1EFC5E2.internal.baidu.com>
2015-10-27 11:18:53 +0800

    黑白边框演示

--- app/src/main/java/com/tjut/wuyifan/cardiogram/MainActivity.java
+++ app/src/main/java/com/tjut/wuyifan/cardiogram/MainActivity.java
@@ -7,50 +7,55 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.opencsv.CSVWriter;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_TAKE_PHOTO = 1;
     private static final int REQUEST_CUT_PHONTO = 2;
     private static final String TAG = "Cardiogram";
-    private static final String IMAG = "/storage/emulated/0/Pictures/JPEG_20151023_155759_-832276725.jpg";
-
+    //黑白框边界的单位长度
+    private static final int BORDER = 30;
+    //用来显示图片
     private ImageView mImageView = null;
+    //拍照键
     private FloatingActionButton mFabCamera = null;
-    private FloatingActionButton mFabCut = null;
-    private FloatingActionButton mFabPlay = null;
     private Bitmap mBitmap = null;
+    //存储已拍照片或已剪裁照片的路径
+    private String mCurrentPhotoPath = null;
 
-    private static String mCurrentPhotoPath = null;
 
+    //opencv是一个图像处理的库，异步加载opencv
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -70,8 +75,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //加载布局
         setContentView(R.layout.activity_main);
 
+        //初始化控件
         mImageView = (ImageView) findViewById(R.id.image);
 
         mFabCamera = (FloatingActionButton) findViewById(R.id.fab_camera);
@@ -81,26 +88,6 @@ public class MainActivity extends AppCompatActivity {
                 dispatchTakePictureIntent();
             }
         });
-
-        mFabPlay = (FloatingActionButton) findViewById(R.id.fab_play);
-        mFabPlay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(mBitmap != null) {
-                    new ExtractTask().execute(mBitmap);
-                }
-            }
-        });
-
-        mFabCut = (FloatingActionButton) findViewById(R.id.fab_save);
-        mFabCut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new LoadImageFromFileTask().execute(IMAG);
-            }
-        });
-
-
     }
 
     @Override
@@ -109,6 +96,7 @@ public class MainActivity extends AppCompatActivity {
         OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_11, this, mLoaderCallback);
     }
 
+    //调用本地的相机app，开始拍照，所拍照片存在mCurrentPhotoPath路径下
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -125,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //调用本地的截图app，开始截图，所截照片存在mCurrentPhotoPath路径下
     private void dispatchCutPictureIntent() {
         Intent cutPictureIntent = new Intent("com.android.camera.action.CROP");
         File photoFile = null;
@@ -141,6 +130,7 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(cutPictureIntent, REQUEST_CUT_PHONTO);
     }
 
+    //根据时间生成图片名
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -157,6 +147,8 @@ public class MainActivity extends AppCompatActivity {
         return image;
     }
 
+
+    //调用本地相机或截图app结束后的回调事件，拍照完后开始截图，截图完后开始加边框并显示
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
@@ -173,7 +165,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //异步加载本地图片
+    //异步加载剪裁后的图片，即mCurrentPhotoPath路径下的图片
     private class LoadImageFromFileTask extends AsyncTask<String, Void, Bitmap> {
 
         @Override
@@ -182,14 +174,15 @@ public class MainActivity extends AppCompatActivity {
 
             if (file.exists()){
                 Bitmap bitmap =  BitmapFactory.decodeFile(file.getAbsolutePath());
-                int border = 30;
-                int whiteHeight = 2 * border;
-                int blackHeight = 3 * border;
+                //白框、黑框高度和宽度
+                int whiteHeight = 2 * BORDER;
+                int blackHeight = 3 * BORDER;
                 int whiteWidth = bitmap.getWidth();
                 int blackWidth = bitmap.getWidth();
 
                 Bitmap barBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight() + 2 * (whiteHeight + blackHeight), Bitmap.Config.RGB_565);
 
+                //画边框
                 Canvas canvas = new Canvas(barBitmap);
                 Paint paint = new Paint();
                 paint.setColor(Color.BLACK);
@@ -200,12 +193,8 @@ public class MainActivity extends AppCompatActivity {
                 canvas.drawRect(0, blackHeight + whiteHeight + bitmap.getHeight(), whiteWidth, barBitmap.getHeight() - blackHeight, paint);
                 canvas.drawBitmap(bitmap, 0, whiteHeight + blackHeight, null);
 
-//                Matrix matrix = new Matrix();
-//                matrix.setRotate(45);
-
                 return barBitmap;
             }
-
             return null;
         }
 
@@ -215,27 +204,9 @@ public class MainActivity extends AppCompatActivity {
 
             if(bitmap != null) {
                 mBitmap = bitmap;
+                //显示加边框后的图片
                 mImageView.setImageBitmap(bitmap);
             }
-        }
-    }
-
-    private class ExtractTask extends AsyncTask<Bitmap, Void, Void> {
-
-        @Override
-        protected Void doInBackground(Bitmap... bitmaps) {
-            Mat rgbMat = new Mat();
-//            Mat grayMat = new Mat();
-            Utils.bitmapToMat(bitmaps[0], rgbMat);
-//            Imgproc.cvtColor(rgbMat, grayMat, Imgproc.COLOR_RGB2GRAY);
-            Size size = rgbMat.size();
-            for (int i = 0; i < size.height; i++) {
-                for (int j = 0; j < size.width; j++) {
-                    double[] data = rgbMat.get(i, j);
-                    Log.d(TAG, "pixel: (" + data[0] + "," + data[1] + "," + data[2]);
-                }
-            }
-            return null;
         }
     }
 }
---
+++ app/src/main/java/com/tjut/wuyifan/cardiogram/utils/Utility.java
@@ -0,0 +1,7 @@
+package com.tjut.wuyifan.cardiogram.utils;
+
+/**
+ * Created by guogaoyang on 2015/10/26.
+ */
+public class Utility {
+}
